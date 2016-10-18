9823ed02e77036f3f2b8ac2c5ead0f7cf7d950ef
LingchuanHu <lhu2@xiaohongshu.com>
2016-10-18 17:46:35 +0800

    add context check code

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,67 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "smartinputmethod" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        vscode.window.showInformationMessage('1');
     });
 
     context.subscriptions.push(disposable);
+
+
+class InputMethodAdaptor {
+    
+    private _chinese_input = 1;
+    private _english_input = 2;
+    
+    private _is_comment_regex = /$[\#(\/\/)]/;
+    
+    public updateInputMethod () {
+        if (!this._suggest_change_input_method()) {
+            return;
+        }
+        
+    }
+    
+    private _suggest_change_input_method (): Boolean {
+        let editor = vscode.window.activeTextEditor;
+        let line_text = this._current_line_text();
+        let current_char = editor.selection.active.character;
+        
+        // 判断当前光标是否是在comment中
+        if (line_text.match(this._is_comment_regex)) {
+            return false;
+        }
+        // 判断当前光标是否是在字符串中
+        return !this._is_in_a_string(line_text, current_char);
+    }
+    
+    private _is_in_a_string(str: string, position): Boolean {
+        if (!str) {
+            return false;
+        }
+        let is_in_str = false;
+        for (let c of str) {
+            if (c == "'" || c == '"') {
+                is_in_str = !is_in_str;
+            }
+        }
+        return is_in_str;
+    }
+    
+    private _current_line_text (): string {
+        let editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return '';
+        }
+        let document = editor.document;
+        let position = editor.selection.active;
+        let current_line_number = position.line;
+        return document.lineAt(current_line_number).text;
+    }
+    
 }
 
 // this method is called when your extension is deactivated
